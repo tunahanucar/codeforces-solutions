ccd2259e4b1c8b89ccc247a18c4214f8d7ec4470
Mustafa Tunahan Uçar <39824372+tunahanucar@users.noreply.github.com>
2024-12-19 12:05:31 +0300

    Add files via upload

---
+++ easy_problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int line;
+	cin >> line;
+	for(int i = 0; i < line; i++){
+		int num;
+		cin >> num;
+		cout << num-1 << endl;
+	}
+	return 0;
+}
